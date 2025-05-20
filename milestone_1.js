4e85fe771046955c299aab75b7bb6093b035ffa7
nathansungu <nathanamudavi@gmail.com>
2025-05-20 15:41:35 +0300

    completed challenge1 : sum of 2 numbers

---
+++ milestone_1.js
@@ -0,0 +1,24 @@
+//sum of two numbers
+function addition(num1, num2) {
+    let sum = num1+num2;
+   
+    console.log(sum)
+            
+}
+
+//convert minutes into seconds
+
+ function convert(minutes) {
+    let seconds =   60*minutes  
+    return seconds
+ }
+ //perimeter of a rectangle
+
+ function findPerimeter(length, width){
+    let perimeter = 2(length*width);
+    return perimeter;
+ }
+
+ //check negative
+ const isNegative =(number)=> (number<1? true: false)
+
