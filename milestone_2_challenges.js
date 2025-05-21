ed596506ef19805d3d4fb435169d088fc464749e
nathansungu <nathanamudavi@gmail.com>
2025-05-22 02:47:27 +0300

    challengge 2:  find the maximum value

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -1,4 +1,6 @@
 // basic Data Structures
+
+//sum of positives valu in array
 const sumOfPositives =(array)=>{
     let sum =0;
     for( let i=0; i<=array.length-1; i++){
@@ -10,3 +12,15 @@ const sumOfPositives =(array)=>{
 }
 
 console.log(sumOfPositives([1,-4,6]))
+
+//maximum value
+const findMax =(array)=>{
+    var maximum = array[0];
+    for (let i = 1; i < array.length-1; i++) {        
+        if(array[i]>maximum){
+            maximum =array[i];
+        }      
+    }
+    return maximum
+}
+console.log(findMax([10, 7, 2, 9, 5]))
