491dd482d87b2b012f93348f1f110663458682e6
nathansungu <nathanamudavi@gmail.com>
2025-05-21 09:05:17 +0300

    challenge 11: sum of even numbers

--- milestone_1.js
+++ milestone_1.js
@@ -115,3 +115,19 @@ function perimeter(shape,num ) {
 
     
 }
+
+//sum of even numbers
+
+function sumEvenNumbers(n) {
+    let sum =0;
+    for(let i=1; i<=n; i++){
+        
+        if (i%2 ===0) {
+           sum = sum + i;
+            
+        }
+            
+    }
+    return sum
+    
+}
