172cd1198435e0cedcf0461fecb9069e9fd04773
nathansungu <nathanamudavi@gmail.com>
2025-05-22 05:17:12 +0300

    milestone 2 challenge 11: linear search

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -75,69 +75,78 @@ console.log(countProperties({ name: "Alice", age: 25, city: "Paris" }));
 
 //filter by length
 
-const filterByLength =(array, minLength)=>{
-
-    let newArray =[];
-    for(let i=0; i<=array.length-1; i++){
-        if (array[i].length<=minLength) {
-            newArray.push(array[i])            
-        }
-        
+const filterByLength = (array, minLength) => {
+  let newArray = [];
+  for (let i = 0; i <= array.length - 1; i++) {
+    if (array[i].length <= minLength) {
+      newArray.push(array[i]);
     }
-    return newArray
-
-}
-console.log(filterByLength(["cat", "giraffe", "hippo", "dog", "elephant"], 5))
+  }
+  return newArray;
+};
+console.log(filterByLength(["cat", "giraffe", "hippo", "dog", "elephant"], 5));
 
 //sum of even numbers
-const sumEvenNumbers =(array)=>{
-    let sum =0;
-    for(let i=0; i<array.length; i++){
-        if(array[i]%2 ==0){
-            sum =+array[i]
-        }
+const sumEvenNumbers = (array) => {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 == 0) {
+      sum = +array[i];
     }
-    return sum
-}
+  }
+  return sum;
+};
 
 //diffrence between even and odd numbers
-const differenceEvenOdd = (array)=>{
-    let evensum = 0;
-    let oddsum = 0;
+const differenceEvenOdd = (array) => {
+  let evensum = 0;
+  let oddsum = 0;
 
-    for(let i =1; i<=array.length-1; i++){
-        if(array[i]%2 ==0){
-            evensum =+array[i];
-        }else{
-            oddsum =+array[i];
-        }
+  for (let i = 1; i <= array.length - 1; i++) {
+    if (array[i] % 2 == 0) {
+      evensum = +array[i];
+    } else {
+      oddsum = +array[i];
     }
-    let diffrence =evensum-oddsum;
-    return diffrence
-
-}
+  }
+  let diffrence = evensum - oddsum;
+  return diffrence;
+};
 
 //count truthy
-const countTruthy =(object)=>{
-    let count =0;
-    for (let key in object) {
-        if(object[key]){
-            count= count+1
-        }
+const countTruthy = (object) => {
+  let count = 0;
+  for (let key in object) {
+    if (object[key]) {
+      count = count + 1;
     }
-    return count
-}
-console.log(countTruthy({ a: 0, b: "hello", c: false, d: 42, e: null }))
+  }
+  return count;
+};
+console.log(countTruthy({ a: 0, b: "hello", c: false, d: 42, e: null }));
 
 //average of numbers
 
-const average =(array) =>{
-    let sum = 0;
-    for( let i=0; i<array.length; i++){
-        sum=+array[i]        
-    }
-    let average = sum/array.length;
-    return average;
-}
+const average = (array) => {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum = +array[i];
+  }
+  let average = sum / array.length;
+  return average;
+};
 
-console.log(average([2, 4, 6, 8]))
+console.log(average([2, 4, 6, 8]));
+
+//linear search
+const linearSearch = (array, number) => {
+  let position = -1;
+  for (let i = 0; i <= array.length - 1; i++) {
+    if (array[i] === number) {
+      position = i;
+    }
+  }
+  return position;
+};
+
+console.log(linearSearch([5, 3, 7, 1, 4], 10));
