a2a8446b4eb9fbfd57c4855ebba52b2d9bca0e1b
nathansungu <nathanamudavi@gmail.com>
2025-05-22 03:18:15 +0300

    challenge 3 :milestone2 Election winner

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -1,26 +1,44 @@
 // basic Data Structures
 
 //sum of positives valu in array
-const sumOfPositives =(array)=>{
-    let sum =0;
-    for( let i=0; i<=array.length-1; i++){
-        if (array[i]>0) {
-            sum+=array[i];            
-        }
+const sumOfPositives = (array) => {
+  let sum = 0;
+  for (let i = 0; i <= array.length - 1; i++) {
+    if (array[i] > 0) {
+      sum += array[i];
     }
-    return sum
-}
+  }
+  return sum;
+};
 
-console.log(sumOfPositives([1,-4,6]))
+console.log(sumOfPositives([1, -4, 6]));
 
 //maximum value
-const findMax =(array)=>{
-    var maximum = array[0];
-    for (let i = 1; i < array.length-1; i++) {        
-        if(array[i]>maximum){
-            maximum =array[i];
-        }      
+const findMax = (array) => {
+  var maximum = array[0];
+  for (let i = 1; i < array.length - 1; i++) {
+    if (array[i] > maximum) {
+      maximum = array[i];
     }
-    return maximum
-}
-console.log(findMax([10, 7, 2, 9, 5]))
+  }
+  return maximum;
+};
+console.log(findMax([10, 7, 2, 9, 5]));
+
+//Election winner
+const findWinner = (object) => {
+  let winner = object[0];
+
+  for (let i = 1; i < object.length - 1; i++) {
+    if (object[i].votes > winner.votes) {
+      winner = object[i];
+    }
+  }
+  return winner;
+};
+const candidates = [
+  { name: "Alice", votes: 50 },
+  { name: "Bob", votes: 75 },
+  { name: "Charlie", votes: 65 },
+];
+console.log(findWinner(candidates));
